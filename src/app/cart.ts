6ba31e67496ca175e4d79e769e78f56ebfa78ae9
abdelkabir jabrane <abdelkabir.jabrane20@gmail.com>
2025-06-11 21:07:06 +0100

    commit model and auth

---
+++ src/app/cart.ts
@@ -0,0 +1,9 @@
+export interface Cart {
+  id: string; // Ajouter la propriété pour l'ID du panier
+  email: string;
+  productId: string;
+  quantity: number;
+  productName: string;
+  price: number;
+  image: string;
+}
--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Cart } from './cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+  private apiUrl = 'http://localhost:3000/carts'; //
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+
+  private groupedQuantity: any[] = [];
+
+  setGroupedQuantity(quantity: any[]): void {
+    this.groupedQuantity = quantity;
+  }
+
+  getGroupedQuantity(): any[] {
+    return this.groupedQuantity;
+  }
+
+  getGroupedCartItemsByUser(userEmail: string): Observable<Cart[]> {
+    return this.http.get<Cart[]>(`${this.apiUrl}?email=${userEmail}`);
+  }
 }
